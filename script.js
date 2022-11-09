994ae7be11e61d691a2802f34f19ca1d3d82a6ad
ameliamuzzarelli <amuzzarelli@my.dom.edu>
2022-11-08 20:17:25 -0500

    update script and index

--- script.js
+++ script.js
@@ -3,42 +3,48 @@ const data = [
 		title: "Hitler’s Pink Triangles",
 		time: [1933, 1945],
 		region: ["europe"],
-		keyword: ["germany", "holocaust", "europe", "nazi", "pink triangle", "paragraph 175"]
+		keyword: ["germany", "holocaust", "europe", "nazi", "pink triangle", "paragraph 175"],
+		content: [ 'During World War II, Hitler’s rise to power caused both horrific genocide and stunning displacement. In western cultural consciousness, the main people group targeted were European Jews. However, there were many other segments of society that were targeted as well, including the Romani people, Jehovah’s Witnesses, political dissidents, and LGBTQ people, especially gay men. Where Jews were labeled with a Star of David, those identified as gay were labeled with a pink triangle. An article written in the Journal of Homosexuality records that in camps like Buchenwald, those labeled as homosexual were generally lowest in the camp hierarchy, subjected to medical experimentation and service to guards. (Waxman, 2018)', 'The criminalization of alternative sexual orientations has been enshrined in German law since the early 1800s, but the enforcement of them became very real for German citizens in 1935 when the Gestapo began keeping “pink lists” and counting any “suggestive” actions between men as violations of anti-gay laws. (holocaust museum) The number of people who were displaced as part of these events is estimated to be around 10,000. (Lautman, 1980)' ]
 	},
 
 	{
 		title: "Involuntary Commitment",
 		time: [1935, 1974],
 		region: ["europe"],
-		keyword: ["england", "cure", "europe", "commitment", "institutionalization", "hospitalization"]
+		keyword: ["england", "cure", "europe", "commitment", "institutionalization", "hospitalization"],
+		content: ['In the west, there is a history of involuntary commitment of LGBTQ people by their families or communities. This can take the form of forcing the queer person into treatment via a mental health facility or a conversion camp, where extreme measures are taken to cure the person of their sexual orientation. The pathologization that led to this form of dislocation is obvious: homosexuality was a disease to be cured using “medical” treatment.  Though generally temporary, there is an added element of persecution in that the methods used to attempt to cure these individuals were often barbaric by modern medical standards. (Dickinson, 2015)', 'In his book Curing Queers: Mental Nurses and Their Patients 1935-74, Tommy Dickinson uses primary accounts of those involved in conversion programs in England during the mid-twentieth century. He credits World War II with a great deal of the circumstances leading up to this rise in forced institutionalization, including increases in both the prevalence of  men in gay relationships and an increase in their punishment. According to several interviewees and military records, both the United States Army and the British Armed Forces saw a marked increase in romantic and sexual relationships between men. Being caught with another man resulted in court martial proceedings, which multiplied exponentially as the war went on due to the employment of army psychiatrists tasked with identifying homosexual soldiers using standardized diagnostic tests. Outside of mobilized army environments, forced commitment was routinely achieved through the efforts of professionals such as teachers and doctors who referred people who had confided in them about alternative sexual orientations or desires for treatment.(Smith, Bartlett, and King, 2004) Throughout the 1950’s and 1960’s, entrapment by police became increasingly common, with young male undercover officers spending time in public bathrooms waiting to be propositioned so that they could either arrest the individual or forcibly commit them to an institution. (Dickinson, 2015)', 'Once taken from their homes, friends, family, and community, patients rarely had any idea of when or whether they would be released. Treatment was generally a form of aversion therapy, in which a practitioner would present the patient with a reminder or image of homosexual sex acts and couple it with torture (electrocution, induced nausea, or molestation were used most often) so that the patient would hopefully begin to associate homosexuality with pain, fear, and disgust. (Smith, Bartlett, and King, 2004) Those that were dislocated to treatment centers indefinitely described their experiences as akin to Nazi practice of imprisoning and torturing homosexual men in concentration camps. One man, identified as Percival Thatcher, described the aversion therapy he received as similar to that of those tortured by the Gestapo for information. (Dickinson, 2015) Similarly, during his research, Dickinson found several similarities between the duties of nurses to administer aversion therapy and keep the details confidential in both UK mental hospitals during this time frame and nurses in Nazi Germany who were part of the push eliminate gay and transgender men. (Dickinson, 2015)' ]
 	},
 
 	{
 		title: "Immigration Policy",
 		time: [1950, 1970],
 		region: ["americas"],
-		keyword: ["united states", "americas", "immigration", "deportation", "immigration and nationality act"]
+		keyword: ["united states", "americas", "immigration", "deportation", "immigration and nationality act"],
+		content: ['One of the ways the United States engaged in queer dislocation is through the immigration system, enshrining in the law their belief about alternative sexualities and their status as a pathology. Most prevalent during the 1950’s and 1960’s, anti-LGBTQ immigration policies resulted in the deportation of many queer individuals from the United States. The pathologization of queer orientations is cited as the reasoning behind these deportations. While it was not unheard of before this, the 1952 Immigration and Nationality Act was the initial legal basis for the deportation or denial of entry for immigrants that were found to have “psychopathic personalities” or those who would commit “sexual deviancy” in the US. (66 United 82 Congress, 1952)  INA was supported and clarified to more explicitly prohibit known LGBTQ people from entering the US by a 1965 Congressional amendment as well as the 1967 Supreme Court decision of Boutilier v. Immigration and Naturalization Service. Congress amended INA in 1965 to add refine the exclusion to those who had “sexual deviations,” (Stein, 2010) a slight change in wording that removed any burden of proof for the state to prove an immigrant’s intent to enter the country for the purpose of committing “sexual deviancy.”', 'Boutilier vs. INS upheld and further clarified the legality of the deportation of individuals for their sexual identity. (Clark, 1966) In his paper, “All the Immigrants Are Straight, All the Homosexuals Are Citizens, But Some of Us Are Queer Aliens,” Marc Stein argues that this decision not only paved the way for the deportation of people based solely on their sexual orientation, but it also created legal precedent to claim that queer people had a form of psychosis, leading to years of legal discrimination against the queer community. (Stein, 2010) The subject of this case, Clive Boutilier was a Canadian immigrant who had lived in the United States with his parents since childhood, but was deported after admitting to a sodomy charge. Boutilier was deemed to be psychotic and deviant, and this pathologization of Boutilier’s sexual orientation would be used to deport other queer immigrants until 1990. (Stein, 2010)']
 	},
 
 	{
 		title: "AIDS Epidemic",
 		time: [1970, 1990],
 		region: ["americas"],
-		keyword: ["United States", "Americas", "AIDS", "GRID", "Disease", "hospitalization"]
+		keyword: ["United States", "Americas", "AIDS", "GRID", "Disease", "hospitalization"],
+		content: ['Another example of queer dislocation that fits into this non-traditional view of displacement is the Acquired Immunodeficiency Syndrome (AIDS) epidemic of the late twentieth century. Becoming an outbreak in the United States during the 1970’s and 1980’s, widespread fear of the disease caused the already-entrenched pathologization of alternative sexual orientations to worsen. Then know as Gay-Related Immune Deficiency (GRID), there was a belief that the disease was an affliction solely of the gay male community, and sexual contact between men and drug use became the suspected cause of “gay cancer.” (Halkitis, 2014) Coupled with the actual pathology of what would soon be understood as the HIV-1 virus, being gay came to be thought of as a preexisting condition that would lead to AIDS. (Halkitis, 2014)', 'The dislocation caused by the AIDS crisis was quiet and largely ignored. While there was never an official quarantine, much of the public called for one. According to a 1985 New York Times poll, 51% of Americans were in favor of a quarantine for AIDS victims, and an only slightly lower percentage (48%) were in favor of issuing identification to victims to keep track of their movements. (Associated Press, 1985) These conditions did not lead to government-sanctioned quarantines or surveillance, but they did lead to communities that were hostile towards gay men, forcing many out of their hometowns or states. Ralph, one man interviewed by Perry Halkitis for his book The Aids Generation, recalled that he felt lucky to have escaped his home state of Utah after the outbreak had begun. He told Halkitis that after he fled, he began to identify with  stories of Holocaust survivors and their feelings of revilement and necessity to flee. (Halkitis, 2014)', 'The interviews given by these men to Halkitis about their lives during the AIDS epidemic revealed how the rejection by their larger communities often dislocated them. While this was not something they often described as egregious, it was a factor that played into their psyche later in life and informed their ideas about their communities, their families, and their identities.']
 	},
 
 	{
 		title: "Queer Homelessness",
 		time: [1980, 2022],
 		region: ["americas", "europe", "asia", "africa"],
-		keyword: ["Familial rejection", " housing", "foster system"]
+		keyword: ["Familial rejection", " housing", "foster system"],
+		content: ['In a more recent example, there is a documented trend of children who identify themselves as queer being kicked out of their homes after telling their parents or caregivers. Contemporary statistics record that 68% of LGBTQ youth who come out to their parents in the United States are rejected and either enter the foster system or become homeless. (Lesley University School of Human Services, n.d.) While there are no verifiable statistics before the 1990s, contemporaries of the 1980’s expressed concern about the rehoming of these children, (Mallon, 1992)  indicating that the numbers of dislocated queer youth were high enough to cause a documentable problem for child welfare specialists such as Gary Mallon.', 'A largely neglected group when it comes to research at the time, Mallon explains that the numbers of gay and lesbian youths who needed alternative housing were not only growing throughout the 1980’s, but the obstacles involved in providing them housing was increasingly difficult due to structural homophobia. Mallon and his colleagues found themselves struggling to place children due to fear, disgust, or simple rejection due to stated or perceived sexual orientation. Mallon found that this was particularly present in larger adoption and foster agencies, who had decided either explicitly or implicitly not to involve themselves in rehoming or emergency housing cases involving LGBTQ parties. (Mallon, 1992)  Here, traces of the pathologization of alternative sexual orientations still had a heady influence. Coinciding with the AIDS epidemic of the same decades, the fear of non-heterosexual people even extended to at-risk children, leaving them in flux, with no home to return to, and nowhere to go. This situation is highly relatable to primary testimony from displaced individuals all over the world.']
 	},
 
 	{
 		title: "Conversion Efforts",
 		time: [1970, 2022],
 		region: ["americas"],
-		keyword: ["Conversion camps", "conversion therapy", "sexual reeducation"]
+		keyword: ["Conversion camps", "conversion therapy", "sexual reeducation"],
+		content: ['Conversion efforts are a worldwide issue for queer people. While reasons vary, there are large numbers of documented cases all over the world of individuals who are forced into therapy, abusive living situations, or camps intended to “cure” them of their sexual orientation. This leads to significant displacement not in large events in time-bound eras, but rather in constant flow of few refugees at a time.', 'In the US, twenty-two states have no protections on the books to discourage conversion therapy, while twenty have bans on conversion therapy for minors. This leaves a majority of states and localities that implicitly allow these types of practices, making these areas more unsafe for the LGBTQ communities. (Movement Advancement Project, 2022) The UCLA School of Law’s Williams Institute estimates that 698,000 Americans have undergone conversion therapy, with 350,000 having done so under the age of eighteen. (Mallory, Brown, and Conron, 2019) However, these estimates deal only with conversion efforts provided by licensed health care professionals, meaning that there are likely far more efforts that are less regulated than even these few. A major category in the non-professional space would be the efforts of religious groups, which the Williams Institute estimates will attempt to “cure” 57,000 queer youth before they reach the age of 18. Regulation typically does not apply to these types of groups, leaving more room for abuse. (Mallory, Brown, and Conron, 2019)', 'The UK reports significantly fewer instances, but the numbers are still high. Responses from a 2018 survey conducted by the UK government indicated that 2% of the 108,000 respondents had undergone conversion therapy. Those from ‘diverse ethnic backgrounds’ were reported to be twice as likely to have endured it. And 1 in 10 trans respondents had undergone it. Half of these were conducted by faith groups. (Farley and Lawrie, 2021) A ban on conversion therapies is meant to be passed into law for England and Wales soon, but it will not protect transgender citizens. (Gallagher and Parry, 2022)', 'In a detailed report created by the Sexual Orientation, Gender Identity and Expression and Sex Characteristics (SOGIESC) Unit of the Centre for Human Rights of the Faculty of Law, University of Pretoria in South Africa, conversion efforts are alive and well on the African continent. While it is recorded in many countries, it is most prevalent in South Africa, Nigeria, Kenya, Tanzania, and Zambia.  Like in the United States, the brutality of some conversion camps in East Africa cause displacement for those whose families and communities are insistent upon queer individuals’ participation.']
 	},
 
 
@@ -46,7 +52,8 @@ const data = [
 		title: "Modern Legislation",
 		time: [1980, 2022],
 		region: ["americas", "africa"],
-		keyword: ["United States", "Africa", "illegal", "imprisonment", "death penalty"]
+		keyword: ["United States", "Africa", "illegal", "imprisonment", "death penalty"],
+		content: ['While there is not yet data on the outcomes, there is a rise in anti-LGBTQ legislation all over the world. It is especially prevalent in the United States. At the end of March, NBC News reported almost 240 new bills that targeted the freedoms of queer people filed so far in 2022. (Lavietes and Ramos, 2022) Modern legislation also threatens the lives and rights of those in much of the African continent and the Middle East region. (Human Rights Watch, 2022) There are emerging reports, like those related to conversion camps, that individual people are fleeing their areas when their safety is threatened and seeking asylum in more friendly locations. As these more modern legislations begin to proliferate and be signed into law, more information about how they have affected the lives and homes of those affected will come to light as well. ']
 	}
 ]
 
@@ -54,7 +61,7 @@ function generate(record, index) {
 	return `
 		<div class="col-12 col-sm-6 col-md-4 col-lg-3">
 				<div class="card h-100">
-					<img class="card-img-top" alt="Photo of ${record.title}" src="https://via.placeholder.com/500">
+<!--					<img class="card-img-top" alt="Photo of ${record.title}" src="https://via.placeholder.com/500">-->
 					
 					<div class="card-body d-flex flex-column">
 						<h5 class="card-title">${record.title}</h5>
@@ -71,7 +78,7 @@ function generate(record, index) {
 									</div>
 									
 									<div class="modal-body">
-										You're a very talented young man, with your own clever thoughts and ideas. Do you need a manager? Jaguar shark! So tell me - does it really exist? Do you have any idea how long it takes those cups to decompose. They're using our own satellites against us. And the clock is ticking.
+										${fillModal(record.content)}
 									</div>
 								</div>
 							</div>
@@ -145,6 +152,16 @@ function makeModals() {
 			showModal(element);
 		});
 	});
+}
+
+function fillModal(paragraphs) {
+	let result = '';
+
+	paragraphs.forEach(paragraph => {
+		result += `<p>${paragraph}</p>`
+	});
+
+	return result;
 }
 
 function showModal(element) {
